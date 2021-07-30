274d190ed2ff4ad19ff4b46e765d9efcc8181bc0
fatihkklnc <fatihkklnc@gmail.com>
2021-07-30 09:31:50 +0300

    Add project files.

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿
+namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(805, 277);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1});
+            this.dataGridView1.Location = new System.Drawing.Point(40, 38);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(732, 179);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Column1
+            // 
+            this.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Column1.HeaderText = "Column1";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.Width = 54;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.AllowUserToDeleteRows = false;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(40, 295);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.ReadOnly = true;
+            this.dataGridView2.Size = new System.Drawing.Size(732, 143);
+            this.dataGridView2.TabIndex = 3;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(99, 12);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(81, 17);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Hepsini seç";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.Click += new System.EventHandler(this.checkBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(998, 508);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Column1;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,131 @@
+﻿using Intuit.Ipp.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+
+    
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+
+            
+
+        }
+        DataTable tablo = new DataTable();
+        DataTable tablo2 = new DataTable();
+        private void checkboxHeader_CheckedChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        
+        
+
+       
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            
+
+            
+            
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            DataGridViewCheckBoxCell ch1 = new DataGridViewCheckBoxCell();
+            ch1 = (DataGridViewCheckBoxCell)dataGridView1.Rows[dataGridView1.CurrentRow.Index].Cells[0];
+
+            if(ch1.Value == null)
+                ch1.Value = false;
+            
+            switch (ch1.Value.ToString())
+            {
+                case "True":
+                    ch1.Value = false;
+                    checkBox1.Checked = false;
+                    if (dataGridView2.Rows.Count > 0)
+                        for(int i=0;i<dataGridView2.Rows.Count;i++)
+                            if(dataGridView2.Rows[i].Cells[1].Value == dataGridView1.Rows[dataGridView1.CurrentRow.Index].Cells[2].Value)
+                            dataGridView2.Rows.RemoveAt(dataGridView2.Rows[i].Index);
+                    break;
+                case "False":
+                    if (e.RowIndex != -1)
+                    {
+                        ch1.Value = true;
+                        tablo.Rows.Add(dataGridView1.Rows[dataGridView1.CurrentRow.Index].Cells[1].Value, dataGridView1.Rows[dataGridView1.CurrentRow.Index].Cells[2].Value);
+                    }
+                        break;
+            }
+            
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tablo.Columns.Add("ADI", typeof(string));
+            tablo.Columns.Add("NUMARASI", typeof(string));
+            dataGridView2.DataSource = tablo;
+            tablo2.Columns.Add("ADI", typeof(string));
+            tablo2.Columns.Add("NUMARASI", typeof(string));
+            tablo2.Rows.Add("FAtih", "05301505815");
+            tablo2.Rows.Add("FAtih2", "05301505815");
+            dataGridView1.DataSource = tablo2;
+
+        }
+
+        
+        private void checkBox1_Click(object sender, EventArgs e)
+        {
+            DataGridViewCheckBoxCell ch1 = new DataGridViewCheckBoxCell();
+
+            if (checkBox1.Checked == true)
+            {
+                for (int i = 0; i < dataGridView1.Rows.Count; i++)
+                {
+
+                    ch1 = (DataGridViewCheckBoxCell)dataGridView1.Rows[i].Cells[0];
+
+
+                    if (Convert.ToBoolean(ch1.Value) == false)
+                    {
+                        tablo.Rows.Add(dataGridView1.Rows[i].Cells[1].Value, dataGridView1.Rows[i].Cells[2].Value);
+                        ch1.Value = true;
+                    }
+
+                }
+
+            }
+            int dataCount = dataGridView2.Rows.Count;
+            if (checkBox1.Checked == false)
+            {
+                for (int a = 0; a < dataCount; a++)
+                {
+
+                    ch1 = (DataGridViewCheckBoxCell)dataGridView1.Rows[a].Cells[0];
+                    ch1.Value = false;
+
+                    dataGridView2.Rows.RemoveAt(dataGridView2.Rows[0].Index);
+
+
+                }
+
+
+            }
+        }
+    }
+}
